276bf0047ca45e4afdb18b9eaf95977662132773
PeaceOloruntoba <profprincepeace@gmail.com>
2023-01-13 14:33:33 +0100

    initial commit

---
+++ src/Components/Common/Head.jsx
@@ -0,0 +1,17 @@
+import React from 'react'
+
+const Head = () => {
+  return (
+    <>
+        <section className="head">
+            <div className="container flexSB paddingTB">
+                <div className="logo">
+                    <img src="" alt="" />
+                </div>
+            </div>
+        </section>
+    </>
+  )
+}
+
+export default Head
---
+++ src/Components/Common/Header.jsx
@@ -0,0 +1,10 @@
+import React from 'react'
+import Head from './Head'
+
+const Header = () => {
+  return (
+    <Head />
+  )
+}
+
+export default Header
